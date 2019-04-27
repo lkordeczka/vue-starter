9ade7acaf72bde2342b9276c3c0d33edb827834e
student16 <student16@pcoip.ki.agh.edu.pl>
2019-04-27 15:23:40 +0200

    add login component

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,45 @@
 <template>
   <div id="app">
-    <h1>ążźćół</h1>
-    <p>{{ email }}</p> <!-- email jest zmienną szukaną w klasie -->
-    <p>{{ password }}</p>
-    <!-- v-mode pozwala dodać specjalny atrybut vue, v-model="email" pozwala edytować wartość email -->
-    <table>
-      <!-- <tr>
-        <td>
-          <p>mail: </p>
-        </td>
-        <td>
-          <div v-if="email.length < 10">Ale masz krótki adres!</div>
-          <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-          <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-          <input type="text" v-model="email"><button @click="alertMyEmail()"></button>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>password: </p>
-        </td>
-        <td>
-          <input type="text" v-model="password">
-        </td>
-      </tr> -->
-      <tr>
-        <td>
-          <p>Add1: </p>
-        </td>
-        <td>
-          <div v-if="isAuthencited">
-            <div v-if="email.length == 0">Wpisz swój mail</div>
-            <input type="text" v-model="email"><button @click="">Zaloguj się</button>
-          </div>
-          <div v-if="isAuthencited">
-            <div v-else-if="email.length == 0">Wpisz swój mail</div>
-            <input type="text" v-model="email"><button @click="">Zaloguj się</button>
-          </div>          
-        </td>
-      </tr>        
-    </table>
-  
-    <div v-if="isAuthencited">
-      <p>Zaloguj jako</p>
-      <button>
-    </div>
-    
-
-
-
-
+    <h1>Witamy w systemie zapisów</h1>
+      <div v-if="email.length > 0">
+        <p> Zalogowany jako {{email}}</p>
+        <button @click="logMeOut()">Wyloguj</button>      
+      </div>
+      <div v-else>
+        <login-form @login="logMeIn($event)"
+                    button-label = "Zaloguj"
+                    header= "Zaloguj się z użyciem adresu mail">
+        </login-form>                    
+        <login-form @login="logMeIn($event)"
+                    button-label = "Zarejestruj"
+                    header= "Zarejestruj się z użyciem adresu mail">></login-form>
+      </div>
   </div>
+
+<!-- button-label = "2 + 2" wypisze "4 + 4"
+                    :button-label = "2 + 2" wypisze "8". Dwukropek ewaluje wartość stringa na kod -->
 </template>
 
 <script>
-// Odpowiednik klasy odpowiadający za wygląd komponentu
-export default {
-// Odpowiednik metody nieprzyjmującej żadnego argumentu
-  data(){
-    return {
-      email: 'jakisMail@o2.pl',
-      password: 'jakies haslo'
-    }
-  }, //Musi byc przecinek(całe ciało klasy jest obiektem joson)
-  methods: {
-    alertMyEmail() {
-      alert(this.email);
+  import "milligram";
+  import LoginForm from "./LoginForm"
+  
+  export default {
+    components: {LoginForm},
+    data(){
+      return {
+        email: ''
+      };
     },
-    logMeIn(){
-      this.isAuthencited = true;
-    },
-    logMeOut(){
-      this.isAuthencited = false;
-      this.email = '';
+  methods:{
+      logMeIn(username){
+        this.email = username
+      },
+      logMeOut(){
+        this.email = ''
+      }
     }
-
-
-
   }
-}
-
 
 </script>
 
